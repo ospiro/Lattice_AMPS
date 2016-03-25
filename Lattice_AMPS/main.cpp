dee0817f14f137d8a601922f2fa8ffed35604236
Oliver Spiro <Oliver@olivers-macbook-air.local>
2016-03-24 23:02:18 -0400

    Small edits to make parameter changes easier

--- Lattice_AMPS/main.cpp
+++ Lattice_AMPS/main.cpp
@@ -13,8 +13,10 @@ int main()
     int seedRadius = 10;
     double toxinStrength = 4;
     double startingProb[4] = {0.25,0.25,0.25,0.25};
-    double deathRate = 1; //
-    Lattice myLattice(100,startingProb,birthRate,deathRate,seedRadius, 10 , 0.01, toxinStrength, 1);
+    int setYearLength = 4;
+    double setdt = 0.01
+    //double deathRate = 1;
+    Lattice myLattice(100,startingProb,birthRate,seedRadius, setdt, toxinStrength, setYearLength );
     for(int i = 0;i<10;i++)
     {
     myLattice.advanceYear();
